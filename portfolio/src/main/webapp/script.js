2db1aa72620bb1ee1ac0adbc3973656d43ac6b03
Amy <amyyniu@gmail.com>
2020-06-01 10:02:05 -0400

    Fetch a message from the server and add it to the DOM

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -39,3 +39,40 @@ function navbar() {
     navDisplay.style.display = "block";       
   }
 }
+
+/**
+ * Fetches a message from the server and adds it to the DOM.
+ */
+function getRandomQuote() {
+  console.log('Fetching a message.');
+
+  // The fetch() function returns a Promise because the request is asynchronous.
+  const responsePromise = fetch('/data');
+
+  // When the request is complete, pass the response into handleResponse().
+  responsePromise.then(handleResponse);
+}
+
+/**
+ * Handles response by converting it to text and passing the result to
+ * addMessageToDom().
+ */
+function handleResponse(response) {
+  console.log('Handling the response.');
+
+  // response.text() returns a Promise, because the response is a stream of
+  // content and not a simple variable.
+  const textPromise = response.text();
+
+  // When the response is converted to text, pass the result into the
+  // addQuoteToDom() function.
+  textPromise.then(addMessageToDom);
+}
+
+/** Adds a server message to the DOM. */
+function addMessageToDom(quote) {
+  console.log('Adding message to dom: ' + quote);
+
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerText = quote;
+}
