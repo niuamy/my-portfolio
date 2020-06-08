49c798edc3d04733626decb334f4a653feded3e9
Amy <amyyniu@gmail.com>
2020-06-08 17:43:12 +0000

    Add Google Maps to desktop

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -179,8 +179,18 @@ function toggleHeart(heart) {
   }
 }
 
+/**
+ * Creates a map and adds it to the page.
+ */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
 function getMessageDetails() {
   getMessage();
   setMaxLength();
   setSubmitButtonUsability();
+  createMap();
 }
